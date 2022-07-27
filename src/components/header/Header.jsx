9436839cd177e12a9cc6e5259ea3b65e9032b4a6
Bruno Patrício <bpatriciocosta@gmail.com>
2022-07-27 13:22:53 -0300

    Configurando o Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,9 +3,11 @@ import styles from './Header.module.css'
 function Header () {
   return (
     <header className={styles.header}>
-      <h6>Home</h6>
-      <h6>Quem sou eu?</h6>
-      <h6>Contatos</h6>
+      
+        <a href='http://localhost:3000/'>Home</a>
+        <a href='https://www.linkedin.com/in/brunopatricioc/' target="_blank">Quem sou eu?</a>
+        <a href='https://wa.me/5585997275152' target="_blank">Contatos</a>
+            
     </header>
   )
 }
